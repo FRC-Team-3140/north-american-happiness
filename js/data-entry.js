08ad7603e41c3d2924f5af28df05d5c85758971f
Tanner Hobson <thobson@stkfactory.(none)>
2014-02-24 00:02:26 +0000

    Initial code commit

---
+++ js/data-entry.js
@@ -0,0 +1,10 @@
+$("textarea").on("keyup", function(e) {
+    var textarea = $(this);
+    var parent = textarea.parent();
+    console.log(textarea.val());
+    if (textarea.val() !== "") {
+        parent.find("button").hide(1000);
+    } else {
+        parent.find("button").show(1000);
+    }
+});
