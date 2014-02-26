aec275347c2846e9fdaf1a56800671f631de67e2
Tanner Hobson <thobson125@gmail.com>
2014-02-26 04:02:57 +0000

    Added all the actual tables and changed it to read the db settings from a file

--- routes.py
+++ routes.py
@@ -7,6 +7,15 @@ from sqlalchemy.orm.session import sessionmaker, Session
 
 app = Flask(__name__)
 
+# Database_Config is a file which has one value on each line, 
+#+ in order:
+#+ * dbDriver (e.g. mysql+pmysql)
+#+ * dbHostname (e.g. localhost)
+#+ * dbUsername (e.g. magicaldbuser)
+#+ * dbPassword (e.g. supersneakypassword)
+#+ * dbName (e.g. scouting)
+database_config = '/etc/scouting-database-config.txt'
+
 #insert db classes here
 
 Base = declarative_base()
@@ -16,11 +25,12 @@ class sesh(object):
         self.set_Connection()
 
     def set_Connection(self):
-        self.dbDriver   = 'mysql+pymysql'
-        self.dbHostname = ''#REDACTED!
-        self.dbUsername = 'root'
-        self.dbPassword = ''#REDACTED!
-        self.dbName     = ''#REDACTED!
+        with open(database_config, "r") as f:
+            self.dbDriver = f.readline().strip()
+            self.dbHostname = f.readline().strip()
+            self.dbUsername = f.readline().strip()
+            self.dbPassword = f.readline().strip()
+            self.dbName = f.readline().strip()
 
     def create_Session(self):
         Session = sessionmaker()
@@ -31,39 +41,69 @@ class sesh(object):
         self.set_Connection()
         self.engine = create_engine(self.dbDriver + "://" + self.dbUsername + ":" + self.dbPassword + "@" + self.dbHostname + "/" + self.dbName)
 
+class Choice(Base):
+    """
+    CREATE TABLE IF NOT EXISTS scout.choices (
+        id INTEGER NOT NULL AUTO_INCREMENT,
+        choice VARCHAR(1024),
+        PRIMARY KEY (id)
+    );
+    """
+    __tablename__ = 'choices'
+    
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    choice = Column(String(1024))
+    
+    def __repr__(self):
+        return "<Choice(id='%d', choice='%s')>" % (
+            self.id, self.choice)
 
-#template class for sqlalchemy
-'''class class_name(Base):
-    __tablename__ = 'data'
-
-    team_number = Column(Integer, primary_key=True)
-    name     = Column(String)
-    city     = Column(String)
-    info     = Column(String)
-    other    = Column(String)
-    leader   = Column(String)
-    date     = Column(DATE)
-
-    def __init__(self, team_number, name, city, info, other, leader, date)
-        team_number = Column(Integer, primary_key=True)
-        name     = Column(String)
-        city     = Column(String)
-        info     = Column(String)
-        other    = Column(String)
-        leader   = Column(String)
-        date     = Column(DATE)
+class Question(Base):
+    """
+    CREATE TABLE IF NOT EXISTS scout.questions (
+        id INTEGER NOT NULL AUTO_INCREMENT,
+        question VARCHAR(1024),
+        PRIMARY KEY (id)
+    );
+    """
+    __tablename__ = 'questions'
+    
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    question = Column(String(1024))
 
     def __repr__(self):
-        return '<class_name("%s", "%s", "%s", "%s", "%s", "%s", "%s")' % (self.team_number, self.name, self.city, self.info, self.other, self.leader, self.date)
-'''
+        return "<Question(id='%d', question='%s')>" % (
+            self.id, self.question)
+
+class Team(Base):
+    """
+    CREATE TABLE IF NOT EXISTS scout.teams (
+        id INTEGER NOT NULL AUTO_INCREMENT,
+        teamnumber VARCHAR(8),
+        question INTEGER REFERENCES scout.questions,
+        choice INTEGER REFERENCES scout.choices,
+        PRIMARY KEY (id)
+    );
+    """
+    __tablename__ = 'teams'
+    
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    teamnumber = Columns(String)
+    question_id = ForeignKey('questions.id')
+    choice_id = ForeignKey('choices.id')
+    
+    question = relationship('Question', foreign_keys='Team.question_id')
+    choice = relationship('Choice', foreign_keys='Team.choice_id')
+    
+    def __repr__(self):
+        return "<Team(id='%d', teamnumber='%s', question='%s', choice='%s')>" % (
+            self.id, self.teamnumber, self.question, self.choice)
 
 #creating a session
-'''
 dbsession = sesh()
 dbsession.create_Engine()
 dbsession.create_Session()
 teams = dbsession.sess.query(information).all()
-'''
 
 class information(Form):
     #barebones example of how to use wtf forms
@@ -79,4 +119,4 @@ def front_page():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0")
